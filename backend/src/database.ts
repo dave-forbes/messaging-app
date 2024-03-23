e14185a275cb8fee6c210435e2ec8ad751c1745a
David Forbes <davidforbesi@hotmail.com>
2024-03-23 13:55:12 +0000

    change to mongoose mongodb connection

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -1,33 +1,24 @@
+import mongoose from "mongoose";
 import dotenv from "dotenv";
-import { MongoClient, ServerApiVersion } from "mongodb";
 
 dotenv.config();
 
-const uri = process.env.MONGODB_URI;
-
-if (!uri) {
-  console.error("MongoDB URI is not defined.");
-  process.exit(1);
-}
-
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
-
-async function connectDB() {
+const connectDB = async () => {
   try {
-    await client.connect();
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-  } finally {
-    await client.close();
+    if (!process.env.MONGODB_URI) {
+      console.error("MONGODB_URI not found in environment variables");
+      process.exit(1);
+    }
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("Connected to MongoDB");
+  } catch (error: any) {
+    console.error("Error connecting to MongoDB:", error.message);
+    process.exit(1);
   }
-}
+};
 
-export default connectDB;
+const disconnectDB = async () => {
+  await mongoose.disconnect();
+};
+
+export { connectDB, disconnectDB };
