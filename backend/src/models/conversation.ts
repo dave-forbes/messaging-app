60c255c49208d41df7402dab413c02658c8430be
David Forbes <davidforbesi@hotmail.com>
2024-03-24 17:51:40 +0000

    add title to conversation model
    - so easier to identify conversations
    - made title and participants required

--- backend/src/models/conversation.ts
+++ backend/src/models/conversation.ts
@@ -1,13 +1,15 @@
 import mongoose, { Schema } from "mongoose";
 
 interface Conversation {
+  title: string;
   participants: string[]; // Array of user IDs participating in this conversation
   createdAt: Date;
   updatedAt: Date;
 }
 
 const ConversationSchema = new Schema({
-  participants: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  title: { type: String, required: true },
+  participants: [{ type: Schema.Types.ObjectId, ref: "User", required: true }],
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
 });
