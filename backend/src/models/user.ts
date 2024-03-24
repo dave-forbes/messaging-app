3ac8bd24ed420e8f780ab4ee613891abcf3e749c
David Forbes <davidforbesi@hotmail.com>
2024-03-24 10:42:59 +0000

    update user model
    - remove email, presumed not necessary
    - added bio as easy to implement and good for updating user

--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -2,9 +2,9 @@ import mongoose, { Schema } from "mongoose";
 
 interface User {
   username: string;
-  email: string;
   password: string;
   avatar: string;
+  bio: string;
   createdAt: Date;
   updatedAt: Date;
 }
@@ -12,9 +12,9 @@ interface User {
 const UserSchema = new Schema(
   {
     username: { type: String, required: true },
-    email: { type: String, required: true },
     password: { type: String, required: true },
     avatar: { type: String },
+    bio: { type: String },
   },
   { timestamps: true }
 );
