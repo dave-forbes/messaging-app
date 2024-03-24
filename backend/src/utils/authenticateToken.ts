21aa1dff61cb25d63acc5c645b7e9d360db5990a
David Forbes <davidforbesi@hotmail.com>
2024-03-24 12:41:42 +0000

    create function authenticateToken
    - takes request and verifies if it has a valid token

---
+++ backend/src/utils/authenticateToken.ts
@@ -0,0 +1,38 @@
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { NextFunction, Request, Response } from "express";
+
+declare module "express" {
+  interface Request {
+    user?: JwtPayload;
+  }
+}
+
+const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
+  // Check to make sure header is not undefined and token exists
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token === "null" || token == null)
+    return res.status(401).json({
+      message: "Unauthorized: Please log in first.",
+    });
+
+  // Check if JWT_SECRET exists
+  if (!process.env.JWT_SECRET) {
+    return res.status(500).json({
+      message: "Server Error: JWT_SECRET not found in environment variables",
+    });
+  }
+
+  // Verify the JWT token
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      return res.status(403).json({
+        message: "Forbidden: Access forbidden.",
+      });
+    }
+    req.user = user as JwtPayload | undefined;
+    next();
+  });
+};
+
+export default authenticateToken;
