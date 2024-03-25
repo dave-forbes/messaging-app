38cf49a870dcea656e9364e85ddc39f44da03cf3
David Forbes <davidforbesi@hotmail.com>
2024-03-25 09:31:30 +0000

    intial structure
    -with placeholder content, to help visualize

--- frontend/src/components/ConversationList/ConversationList.tsx
+++ frontend/src/components/ConversationList/ConversationList.tsx
@@ -1,3 +1,53 @@
+import styles from "./ConversationList.module.scss";
+
 export default function ConversationList() {
-  return <div></div>;
+  return (
+    <div className={styles.conversationListContainer}>
+      <div className={styles.listWrapper}>
+        <h1 className={styles.title}>Conversations</h1>
+        <ul className={styles.conversationList}>
+          <li className={styles.conversation}>
+            <div className={styles.circle}></div>
+            <div className={styles.conversationContent}>
+              <p>Meghan Ridge</p>
+              <p className={styles.conversationText}>A new Message</p>
+            </div>
+          </li>
+          <li className={styles.conversation}>
+            <div className={styles.circle}></div>
+            <div className={styles.conversationContent}>
+              <p>Ryan James</p>
+              <p className={styles.conversationText}>A new Message</p>
+            </div>
+          </li>
+          <li className={styles.conversation}>
+            <div className={styles.circle}></div>
+            <div className={styles.conversationContent}>
+              <p>Jared Wesley</p>
+              <p className={styles.conversationText}>A new Message</p>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div className={styles.listWrapper}>
+        <h1 className={styles.title}>Contacts</h1>
+        <ul className={styles.conversationList}>
+          <li className={styles.conversation}>
+            <div className={styles.circle}></div>
+            <div className={styles.conversationContent}>
+              <p>Sam Norton</p>
+              <p className={styles.conversationText}>A new Message</p>
+            </div>
+          </li>
+          <li className={styles.conversation}>
+            <div className={styles.circle}></div>
+            <div className={styles.conversationContent}>
+              <p>Alicia Wu</p>
+              <p className={styles.conversationText}>A new Message</p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
