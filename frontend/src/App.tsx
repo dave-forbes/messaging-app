cbfa2ba9c80a7d020cb2e22aeca1748b33a6e87e
David Forbes <davidforbesi@hotmail.com>
2024-03-25 16:43:50 +0000

    update app
    - conditionally render login or app content if user is logged in

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,27 @@
 import ConversationList from "./components/ConversationList/ConversationList";
 import MessageArea from "./components/MessageArea/MessageArea";
 import Navbar from "./components/Navbar/Navbar";
+import { useAuth } from "./contexts/AuthContext";
 import "./styles/global.scss";
 import { useState } from "react";
+import Login from "./components/Login/Login";
 
 export default function App() {
   const [conversationListOpen, setConversationListOpen] = useState(false);
+  const { user } = useAuth();
 
-  // Function to toggle the conversation list visibility
   const toggleConversationList = () => {
     setConversationListOpen(!conversationListOpen);
   };
-  return (
-    <div className="appContainer">
-      <Navbar toggleConversationList={toggleConversationList} />
-      {conversationListOpen && <ConversationList />}
-      <MessageArea />
-    </div>
-  );
+  if (user) {
+    return (
+      <div className="appContainer">
+        <Navbar toggleConversationList={toggleConversationList} />
+        {conversationListOpen && <ConversationList />}
+        <MessageArea />
+      </div>
+    );
+  } else {
+    return <Login />;
+  }
 }
