c809fd845eb3eb9f812fc40958b65bf39d7c9db9
David Forbes <davidforbesi@hotmail.com>
2024-03-25 09:30:33 +0000

    create initial app structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,16 @@
+import ConversationList from "./components/ConversationList/ConversationList";
+import MessageArea from "./components/MessageArea/MessageArea";
+import Navbar from "./components/Navbar/Navbar";
+import "./index.scss";
+
 function App() {
-  return <></>;
+  return (
+    <div className="appContainer">
+      <Navbar />
+      <ConversationList />
+      <MessageArea />
+    </div>
+  );
 }
 
 export default App;
