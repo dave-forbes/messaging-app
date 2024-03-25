2ead430409572d1f150d1301ef08bf825edd3feb
David Forbes <davidforbesi@hotmail.com>
2024-03-25 14:32:14 +0000

    add wrapper
    -to stop message area from growing too wide

--- frontend/src/components/MessageArea/MessageArea.tsx
+++ frontend/src/components/MessageArea/MessageArea.tsx
@@ -10,36 +10,38 @@ export default function MessageArea() {
   const largeIcon = { fontSize: 40 };
   return (
     <div className={styles.messageAreaContainer}>
-      <div className={styles.topDiv}>
-        <div className={styles.flex}>
-          <Avatar />
-          <div>
-            <h1>Ryan James</h1>
-            <p>...typing</p>
+      <div className={styles.messageAreaInnerWrapper}>
+        <div className={styles.topDiv}>
+          <div className={styles.flex}>
+            <Avatar />
+            <div>
+              <h1>Ryan James</h1>
+              <p>...typing</p>
+            </div>
           </div>
+          <MoreVertIcon />
         </div>
-        <MoreVertIcon />
-      </div>
-      <div className={styles.centralDiv}>
-        <p className={styles.date}>Today 12 April </p>
-        <MessageSent
-          content="Hey! I am going to start a book club channel, how that sounds?"
-          createdAt={"13:52"}
-        />
-        <MessageRecieved content="Hello! great, I love the idea!" />
-        <MessageRecieved content="I'm in!" />
-        <MessageSent
-          content="Super, i'll add you shortly"
-          createdAt={"13:52"}
-        />
-        <MessageRecieved content="Thanks! How that meeting went?" />
-      </div>
-      <div className={styles.bottomDiv}>
-        <div className={styles.flex}>
-          <AttachFileIcon sx={largeIcon} />
-          <p>Write a message...</p>
+        <div className={styles.centralDiv}>
+          <p className={styles.date}>Today 12 April </p>
+          <MessageSent
+            content="Hey! I am going to start a book club channel, how that sounds?"
+            createdAt={"13:52"}
+          />
+          <MessageRecieved content="Hello! great, I love the idea!" />
+          <MessageRecieved content="I'm in!" />
+          <MessageSent
+            content="Super, i'll add you shortly"
+            createdAt={"13:52"}
+          />
+          <MessageRecieved content="Thanks! How that meeting went?" />
         </div>
-        <SendIcon sx={largeIcon} />
+        <div className={styles.bottomDiv}>
+          <div className={styles.flex}>
+            <AttachFileIcon sx={largeIcon} />
+            <p>Write a message...</p>
+          </div>
+          <SendIcon sx={largeIcon} />
+        </div>
       </div>
     </div>
   );
