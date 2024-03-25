6ab55dc10ca02cdb54a72dfe9b506bac22b01b21
David Forbes <davidforbesi@hotmail.com>
2024-03-25 09:32:38 +0000

    initial structure
    - with MUI icons

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,3 +1,20 @@
+import styles from "./Navbar.module.scss";
+import ChatIcon from "@mui/icons-material/Chat";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import HearingIcon from "@mui/icons-material/Hearing";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+
 export default function Navbar() {
-  return <div></div>;
+  const smallIcon = { fontSize: 40 };
+  const largeIcon = { fontSize: 50 };
+  return (
+    <div className={styles.navbarContainer}>
+      <HearingIcon sx={largeIcon} />
+      <div className={styles.centralIcons}>
+        <AccountBoxIcon sx={smallIcon} />
+        <ChatIcon sx={smallIcon} />
+      </div>
+      <AccountCircleIcon sx={largeIcon} />
+    </div>
+  );
 }
