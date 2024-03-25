539c3065d12dd4116a680c447f731a54fff48776
David Forbes <davidforbesi@hotmail.com>
2024-03-25 13:19:23 +0000

    update Navbar
    - move svg styles to module.scss
    - add groups icon with onClick to toggle ConversationsList component

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -2,17 +2,19 @@ import styles from "./Navbar.module.scss";
 import ChatIcon from "@mui/icons-material/Chat";
 import HearingIcon from "@mui/icons-material/Hearing";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import GroupsIcon from "@mui/icons-material/Groups";
 
-export default function Navbar() {
-  const smallIcon = { fontSize: 40 };
-  const largeIcon = { fontSize: 50 };
+interface NavbarProps {
+  toggleConversationList: () => void;
+}
+
+export default function Navbar({ toggleConversationList }: NavbarProps) {
   return (
     <div className={styles.navbarContainer}>
-      <HearingIcon sx={largeIcon} />
-      <div className={styles.centralIcons}>
-        <ChatIcon sx={smallIcon} />
-      </div>
-      <AccountCircleIcon sx={largeIcon} />
+      <HearingIcon />
+      <GroupsIcon onClick={toggleConversationList} />
+      <ChatIcon />
+      <AccountCircleIcon />
     </div>
   );
 }
