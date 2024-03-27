2e10aa947b5a35100b49afd2e010990c9728ef8c
David Forbes <davidforbesi@hotmail.com>
2024-03-27 14:11:13 +0000

    update Conversation list
    - remove text and lastreceived props for Conversation, added lastUpdated
    - set conversation list to closed after selecting conversation
    - delete commented out placeholder elements

--- frontend/src/components/ConversationList/ConversationList.tsx
+++ frontend/src/components/ConversationList/ConversationList.tsx
@@ -3,9 +3,11 @@ import Conversation from "../Conversation/Conversation";
 import styles from "./ConversationList.module.scss";
 import { useAuth } from "../../contexts/AuthContext";
 import { useConversation } from "../../contexts/ConversationContext";
+import { useNavbar } from "../../contexts/NavbarContext";
 
 export default function ConversationList() {
   const { user } = useAuth();
+  const { setConversationListOpen } = useNavbar();
   const { setCurrentConversation } = useConversation();
   const [conversations, setConversations] = useState([]);
 
@@ -47,36 +49,13 @@ export default function ConversationList() {
             <Conversation
               key={conversation._id}
               title={conversation.title}
-              lastReceived={conversation.updatedAt.toLocaleString("en-gb")}
-              text="text"
-              onClick={() => setCurrentConversation(conversation)}
+              lastUpdated={conversation.updatedAt}
+              onClick={() => {
+                setCurrentConversation(conversation);
+                setConversationListOpen(false);
+              }}
             />
           ))}
-          {/* <Conversation
-            title="Meghan Ridge"
-            lastReceived="2m"
-            text="Sent a file"
-          />
-          <Conversation
-            title="Ryan James"
-            lastReceived="1h"
-            text="Thanks! How the meeting went?"
-          />
-          <Conversation
-            title="Jared Wesley"
-            lastReceived="4h"
-            text="You: Got it, I'll let you know, no worries"
-          />
-          <Conversation
-            title="Sam Norton"
-            lastReceived="1d"
-            text="Haha, Yeah, sounds good! Thanks so much"
-          />
-          <Conversation
-            title="Alicia Wu"
-            lastReceived="1d"
-            text="Sure, that's completely fine. Don't forget to sign up"
-          /> */}
         </ul>
         <button className={styles.button}>Create</button>
       </div>
