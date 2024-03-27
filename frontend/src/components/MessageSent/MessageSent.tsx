a7a0bf21f0e8c1e934f3d6d091b38f4ee38df11c
David Forbes <davidforbesi@hotmail.com>
2024-03-27 17:09:30 +0000

    remove message info
    - may at back later

--- frontend/src/components/MessageSent/MessageSent.tsx
+++ frontend/src/components/MessageSent/MessageSent.tsx
@@ -1,21 +1,20 @@
 import styles from "./MessageSent.module.scss";
-import DoneAllIcon from "@mui/icons-material/DoneAll";
+// import DoneAllIcon from "@mui/icons-material/DoneAll";
 
 interface MessageSentProps {
   content: string;
-  createdAt: string;
 }
 
-export default function MessageSent({ content, createdAt }: MessageSentProps) {
+export default function MessageSent({ content }: MessageSentProps) {
   return (
     <>
       <div className={styles.messageSent}>
         <div className={styles.messageContent}>
           <p>{content}</p>
-          <div className={styles.messageInfo}>
+          {/* <div className={styles.messageInfo}>
             <p>{createdAt}</p>
             <DoneAllIcon />
-          </div>
+          </div> */}
         </div>
       </div>
     </>
