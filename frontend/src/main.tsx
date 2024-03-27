f01210c3df8115e710cb90bb707c021ce67f065f
David Forbes <davidforbesi@hotmail.com>
2024-03-27 12:22:43 +0000

    wrap app in navbar and conversation contexts

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,11 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { AuthProvider } from "./contexts/AuthContext.tsx";
+import { ConversationProvider } from "./contexts/ConversationContext.tsx";
+import { NavbarProvider } from "./contexts/NavBarContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AuthProvider>
-      <App />
+      <NavbarProvider>
+        <ConversationProvider>
+          <App />
+        </ConversationProvider>
+      </NavbarProvider>
     </AuthProvider>
   </React.StrictMode>
 );
