7a1552edf7850beca5e156e5554c84b07c1c8d2a
David Forbes <davidforbesi@hotmail.com>
2024-03-29 17:18:23 +0000

    update main
    - create router for app, so that i users can navigate to login, register and app pages separately

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,13 +4,35 @@ import App from "./App.tsx";
 import { AuthProvider } from "./contexts/AuthContext.tsx";
 import { ConversationProvider } from "./contexts/ConversationContext.tsx";
 import { NavbarProvider } from "./contexts/NavbarContext.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./components/Login/Login.tsx";
+import Register from "./components/Register/Register.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/app",
+    element: <App />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AuthProvider>
       <NavbarProvider>
         <ConversationProvider>
-          <App />
+          <RouterProvider router={router} />
         </ConversationProvider>
       </NavbarProvider>
     </AuthProvider>
