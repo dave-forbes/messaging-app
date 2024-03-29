1560f43cd534bc9fa4be8f11fb9abdfdb4bdb49a
David Forbes <davidforbesi@hotmail.com>
2024-03-29 11:10:52 +0000

    update create message route
    - push new message to relevant conversation.

--- backend/src/controllers/messageController.ts
+++ backend/src/controllers/messageController.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { NextFunction, Request, Response } from "express";
 import { MessageModel } from "../models/message";
+import { ConversationModel } from "../models/conversation";
 const router = express.Router();
 
 // get all messages for a specifc conversation
@@ -36,10 +37,20 @@ router.post(
         content,
       });
 
+      const updatedConversation = await ConversationModel.findByIdAndUpdate(
+        conversationId,
+        {
+          $push: { messages: newMessage._id },
+          $set: { updatedAt: new Date() }, // Set the updatedAt field to the current date/time
+        },
+        { new: true }
+      ).populate("participants");
+
       res.status(200).json({
         success: true,
-        message: " Message created successfully",
+        message: "Message created successfully",
         newMessage: newMessage,
+        updatedConversation: updatedConversation, // Optionally, you can send back the updated conversation
       });
     } catch (error: any) {
       console.log(error);
