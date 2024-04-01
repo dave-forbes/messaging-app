6f9de23e0e913a076d568816dfa58965df145841
David Forbes <davidforbesi@hotmail.com>
2024-04-01 14:32:10 +0100

    create AppInfo component
    - with initial styling

---
+++ frontend/src/components/AppInfo/AppInfo.tsx
@@ -0,0 +1,24 @@
+import HearingIcon from "@mui/icons-material/Hearing";
+import styles from "./AppInfo.module.scss";
+
+export default function AppInfo() {
+  return (
+    <div className={styles.appInfoContainer}>
+      <HearingIcon />
+      <h1>Welcome to Discussr</h1>
+      <p>
+        This was created for the Odin Project Messaging app assignment, made
+        using React/Vite for the frontend and a NodeJs/Express Rest API backend.
+      </p>
+      <p>
+        As the backend is a RESTful API, realtime updates are not possible, so
+        notifications can't be sent to other users when sending messages.
+      </p>
+      <p>
+        Head over to the github{" "}
+        <a href="https://github.com/dave-forbes/messaging-app">repo</a> for more
+        information on how this app works.
+      </p>
+    </div>
+  );
+}
