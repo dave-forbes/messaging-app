9e3a33b8880d683d2811c90f173eed7c84c2b43c
David Forbes <davidforbesi@hotmail.com>
2024-04-01 15:44:55 +0100

    update cors options

--- backend/src/utils/corsOptions.ts
+++ backend/src/utils/corsOptions.ts
@@ -1,5 +1,5 @@
 const corsOptions = {
-  origin: ["https://main--discussr.netlify.app", "http://localhost:5173"],
+  origin: ["https://discussr.netlify.app", "http://localhost:5173"],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
