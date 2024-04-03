94e0de79743ce5aee99f434a7f4c81ebaad75185
David Forbes <davidforbesi@hotmail.com>
2024-04-03 19:56:10 +0100

    add size prop to determine avatar size

--- frontend/src/components/Avatar/Avatar.tsx
+++ frontend/src/components/Avatar/Avatar.tsx
@@ -3,11 +3,12 @@ import styles from "./Avatar.module.scss";
 
 interface AvatarProps {
   user: UserI | null;
+  size: number;
 }
 
-export default function Avatar({ user }: AvatarProps) {
+export default function Avatar({ user, size }: AvatarProps) {
   return (
-    <div className={user ? styles.avatar : styles.defaultAvatar}>
+    <div className={styles.avatar} style={{ width: size, height: size }}>
       <img src={user?.avatar} alt={`A picture of ${user?.username}`} />
     </div>
   );
