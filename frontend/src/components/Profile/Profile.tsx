d7e878d1859d0f93dc156d5bf78e9610b7e62b8b
David Forbes <davidforbesi@hotmail.com>
2024-04-03 21:13:29 +0100

    modified profile to fetch user data
    - with error handling and loading state

--- frontend/src/components/Profile/Profile.tsx
+++ frontend/src/components/Profile/Profile.tsx
@@ -1,13 +1,43 @@
+import { useEffect, useState } from "react";
 import { useAuth } from "../../contexts/AuthContext";
 import { useConversation } from "../../contexts/ConversationContext";
 import { useNavbar } from "../../contexts/NavbarContext";
 import Avatar from "../Avatar/Avatar";
 import styles from "./Profile.module.scss";
+import apiFetch from "../../utils/apiFetch";
+import API_URL from "../../utils/apiConfig";
+import { UserI } from "../../interfaces/interfaces";
+import CircularProgress from "@mui/material/CircularProgress";
 
 export default function Profile() {
   const { setCurrentConversation } = useConversation();
   const { logout, user } = useAuth();
   const { setIsProfileOpen, setIsUpdateProfileOpen } = useNavbar();
+  const [error, setError] = useState("");
+  const [userData, setUserData] = useState<UserI | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
+  const fetchUser = async () => {
+    try {
+      const data = await apiFetch(
+        `${API_URL}/users/${user?._id}`,
+        {},
+        user?.token,
+        "GET",
+        true
+      );
+      setUserData(data);
+      setError("");
+      setLoading(false);
+    } catch (error: any) {
+      setError(error.toString());
+      setLoading(false);
+    }
+  };
 
   const handleLogoutClick = () => {
     logout();
@@ -17,9 +47,10 @@ export default function Profile() {
 
   return (
     <div className={styles.profileContainer}>
-      <Avatar user={user} size={150} />
-      <h1>{user?.username}</h1>
-      <p>{user?.bio}</p>
+      {loading && <CircularProgress />}
+      <Avatar user={userData} size={150} />
+      <h1>{userData?.username}</h1>
+      <p>{userData?.bio}</p>
       <div className={styles.profileControls}>
         <button className="button" onClick={handleLogoutClick}>
           Logout
@@ -28,6 +59,7 @@ export default function Profile() {
           Update profile
         </button>
       </div>
+      <p>{error}</p>
     </div>
   );
 }
