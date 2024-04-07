2c7897ed55b37084178d3f36b0c6ac916d3f5dad
David Forbes <6923901+dave-forbes@users.noreply.github.com>
2024-04-07 22:13:35 +0100

    update multer storage

--- backend/src/utils/multerSetup.ts
+++ backend/src/utils/multerSetup.ts
@@ -1,17 +1,6 @@
-import path from 'path';
 import multer from 'multer';
 
-const parentDir = path.resolve(__dirname, '../..');
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, path.join(parentDir, 'public/img'));
-  },
-  filename: function (req, file, cb) {
-    cb(null, `${Date.now()}--${file.originalname}`);
-  },
-});
-
+const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
-export default upload;
+export { upload };
