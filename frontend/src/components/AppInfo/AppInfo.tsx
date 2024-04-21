44d6a19073fef7f4e69bf635171b8c47ac09dbae
David Forbes <6923901+dave-forbes@users.noreply.github.com>
2024-04-21 13:15:09 +0100

    add div to set max height

--- frontend/src/components/AppInfo/AppInfo.tsx
+++ frontend/src/components/AppInfo/AppInfo.tsx
@@ -6,23 +6,25 @@ export default function AppInfo() {
     <div className={styles.appInfoContainer}>
       <HearingIcon />
       <h1>Welcome to Discussr</h1>
-      <p>
-        This was created for the Odin Project Messaging app
-        assignment, made using React/Vite for the frontend and a
-        NodeJs/Express Rest API backend.
-      </p>
-      <p>
-        As the backend is a RESTful API, realtime updates are not
-        possible, so notifications can't be sent to other users when
-        sending messages.
-      </p>
-      <p>
-        Head over to the github{' '}
-        <a href="https://github.com/dave-forbes/messaging-app">
-          repo
-        </a>{' '}
-        for more information on how this app works.
-      </p>
+      <div className={styles.appInfoText}>
+        <p>
+          This was created for the Odin Project Messaging app
+          assignment, made using React/Vite for the frontend and a
+          NodeJs/Express Rest API backend.
+        </p>
+        <p>
+          As the backend is a RESTful API, realtime updates are not
+          possible, so notifications can't be sent to other users when
+          sending messages.
+        </p>
+        <p>
+          Head over to the github{' '}
+          <a href="https://github.com/dave-forbes/messaging-app">
+            repo
+          </a>{' '}
+          for more information on how this app works.
+        </p>
+      </div>
     </div>
   );
 }
