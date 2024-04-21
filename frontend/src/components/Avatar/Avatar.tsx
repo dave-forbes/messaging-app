3c1192c7179cc7d01eade0dae94695a23ab58f77
David Forbes <6923901+dave-forbes@users.noreply.github.com>
2024-04-21 12:55:57 +0100

    update avatar
    - fetch profile data to get s3 image url

--- frontend/src/components/Avatar/Avatar.tsx
+++ frontend/src/components/Avatar/Avatar.tsx
@@ -1,5 +1,8 @@
+import { useEffect, useState } from 'react';
 import { UserI } from '../../interfaces/interfaces';
 import styles from './Avatar.module.scss';
+import apiFetch from '../../utils/apiFetch';
+import API_URL from '../../utils/apiConfig';
 
 interface AvatarProps {
   user: UserI | null;
@@ -7,18 +10,43 @@ interface AvatarProps {
 }
 
 export default function Avatar({ user, size }: AvatarProps) {
+  const [userData, setUserData] = useState<UserI | null>(null);
+  if (user) {
+    const userLocalStorage = localStorage.getItem('user');
+    let token: string;
+    if (userLocalStorage) {
+      const obj = JSON.parse(userLocalStorage);
+      token = obj.token;
+    }
+
+    useEffect(() => {
+      fetchUser();
+    }, []);
+
+    const fetchUser = async () => {
+      try {
+        const data = await apiFetch(
+          `${API_URL}/users/${user?._id}`,
+          {},
+          token,
+          'GET',
+          true
+        );
+        setUserData(data);
+      } catch (error: any) {
+        console.error(error.toString());
+      }
+    };
+  }
+
   return (
     <div
       className={styles.avatar}
       style={{ width: size, height: size }}
     >
       <img
-        src={
-          user?.avatar
-            ? user.avatar
-            : 'https://via.placeholder.com/150'
-        }
-        alt={`A picture of ${user?.username}`}
+        src={userData?.avatar}
+        alt={`A picture of ${userData?.username}`}
       />
     </div>
   );
