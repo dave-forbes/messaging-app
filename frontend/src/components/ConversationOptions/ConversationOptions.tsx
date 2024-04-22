ff7fa667a5272c9842670e9dfec3c5ca3213c4f1
David Forbes <6923901+dave-forbes@users.noreply.github.com>
2024-04-22 15:14:19 +0100

    comment out add and remove user functions
    -temporarily

--- frontend/src/components/ConversationOptions/ConversationOptions.tsx
+++ frontend/src/components/ConversationOptions/ConversationOptions.tsx
@@ -1,21 +1,21 @@
 import styles from './ConversationOptions.module.scss';
 import CloseIcon from '@mui/icons-material/Close';
 import { useNavbar } from '../../contexts/NavbarContext';
-import Select from 'react-select';
-import makeAnimated from 'react-select/animated';
-import { useAuth } from '../../contexts/AuthContext';
-import { useState } from 'react';
+// import Select from 'react-select';
+// import makeAnimated from 'react-select/animated';
+// import { useAuth } from '../../contexts/AuthContext';
+// import { useState } from 'react';
 
 export default function ConversationOptions() {
   const { setIsConversationOptionsOpen } = useNavbar();
-  const { user } = useAuth();
-  const [title, setTitle] = useState('');
-  const [selectedUsers, setSelectedUsers] = useState<[] | any>([]);
-  const [users, setUsers] = useState<
-    { value: string; label: string }[]
-  >([]);
-  const animatedComponents = makeAnimated();
-  const [error, setError] = useState('');
+  // const { user } = useAuth();
+  // const [title, setTitle] = useState('');
+  // const [selectedUsers, setSelectedUsers] = useState<[] | any>([]);
+  // const [users, setUsers] = useState<
+  //   { value: string; label: string }[]
+  // >([]);
+  // const animatedComponents = makeAnimated();
+  // const [error, setError] = useState('');
   return (
     <div className={styles.conversationOptionsContainer}>
       <CloseIcon
@@ -23,7 +23,7 @@ export default function ConversationOptions() {
         onClick={() => setIsConversationOptionsOpen(false)}
       />
       <h1>Conversation Options</h1>
-      <div className={styles.inputGroup}>
+      {/* <div className={styles.inputGroup}>
         <label>Add participants:</label>
         <Select
           closeMenuOnSelect={false}
@@ -50,7 +50,8 @@ export default function ConversationOptions() {
           }
           required
         />
-      </div>
+      </div> */}
+      <p>This action cannot be undone.</p>
       <button className="button">Leave Group</button>
     </div>
   );
